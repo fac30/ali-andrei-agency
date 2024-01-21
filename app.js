5d561874f1147c503f22384dcbd750a5f20baddd
Alaa <alaahan80@icloud.com>
2024-01-21 18:00:50 +0000

    App updated

--- app.js
+++ app.js
@@ -1,33 +1,21 @@
 // DARK MODE JS
 
-( () => {
-	let theme = 'dark';
+const toggleDarkMode = () => {
+  const bodyElement = document.body;
+  bodyElement.classList.toggle("dark-mode");
 
-	/**
-	 * Function to toggle between themes
-	 *
-	 * @param {MouseEvent} event
-	 */
-	const onToggleTheme = ( { currentTarget } ) => {
-		const newTheme = theme === 'light' ? 'dark' : 'light';
-		theme = newTheme;
+  const isDarkMode = bodyElement.classList.contains("dark-mode");
+  document
+    .querySelector(".theme-switch")
+    .setAttribute("aria-pressed", isDarkMode);
+};
 
-		document.documentElement.setAttribute( 'data-theme', newTheme );
-		currentTarget.setAttribute( 'aria-pressed', newTheme === 'dark' );
-	}; 
+// Adding click event listener
+const themeSwitchButton = document.querySelector(".theme-switch");
 
-	// Get switch button and add click event
-	const btn = document.querySelector( `.theme-switch` );
-	if ( ! btn ) return;
-	btn.addEventListener( 'click', onToggleTheme, false );
-  
-  
-  // DEMO
-	const toggle = document.querySelector( `.writing-toggle` );
-  if ( ! toggle ) return;
-  toggle.addEventListener('click', () => document.body.classList.toggle('vertical'));
-} )();
-
+if (themeSwitchButton) {
+  themeSwitchButton.addEventListener("click", toggleDarkMode);
+}
 
 //   NAVBAR JS
 
@@ -73,72 +61,41 @@ document
     // Hide the form
     document.getElementById("formContainer").style.display = "none";
   });
+/*=================================*/
 
-// ===================================================================CONTENT RANDOMIZERS=========================================================
-  // =================================================================CHANGING H1s==============================================================
-
-// Object containing catchphrases 
+// Object containing catchphrases
 const catchphrases = {
-    1: 'Train strike?',
-    2: 'Stuck in traffic?',
-    3: 'Plane delayed?',
-    4: 'Long bus journeys?',
-    5: 'Not a huge walker?',
-    6: 'Flat tire?',
-    // Add more catchphrases as needed
-  };
-
-  // Function to inject a random catchphrase into an element with the specified class
-  function injectRandomCatchphrase() {
-    // Get the element with the class "h1--changing"
-    const h1Element = document.querySelector('.h1--changing');
-
-    // Get an array of all catchphrase keys
-    const catchphraseKeys = Object.keys(catchphrases);
-
-    // Choose a random catchphrase key
-    const randomKey = catchphraseKeys[Math.floor(Math.random() * catchphraseKeys.length)];
-
-    // Get the catchphrase message based on the random key
-    const randomCatchphrase = catchphrases[randomKey];
-
-    // Set the text content of the element to the random catchphrase
-    h1Element.textContent = randomCatchphrase;
-  }
-
-  // Call the function to inject a random catchphrase when the page loads
-  window.addEventListener('load', injectRandomCatchphrase);
-
-  // =================================================================CHANGING H2s==============================================================
-
-// Object containing catchphrases 
+  1: "Train strike?",
+  2: "Stuck in traffic?",
+  3: "Plane delayed?",
+  4: "Long bus journeys?",
+  5: "Not a huge walker?",
+  6: "Flat tire?",
+  // Add more catchphrases as needed
+};
 const catchphrases2 = {
-    1: 'We know..',
-    2: 'Us too..',
-    3: 'We get you!',
-    4: 'Not today!',
-    5: 'No worries!',
-    6: 'We have the solution',
-    // Add more catchphrases as needed
-  };
+  1: "We know..",
+  2: "Us too..",
+  3: "We get you!",
+  4: "Not today!",
+  5: "No worries!",
+  6: "We have the solution",
+  // Add more catchphrases as needed
+};
+const h1Element = document.querySelector(".h1--changing");
+const h2Element = document.querySelector(".h2--changing");
 
-  // Function to inject a random catchphrase into an element with the specified class
-  function injectRandomCatchphrase2() {
-    // Get the element with the class "h2--changing"
-    const h2Element = document.querySelector('.h2--changing');
+const randomKey = (phrase) =>
+  Math.floor(Math.random() * Object.keys(phrase).length) + 1;
+const changeCatchphrase = () => {
+  h1Element.textContent = catchphrases[randomKey(catchphrases)];
+  h2Element.textContent = catchphrases2[randomKey(catchphrases2)];
+};
 
-    // Get an array of all catchphrase keys
-    const catchphraseKeys = Object.keys(catchphrases2);
+// changeCatchphrase();
 
-    // Choose a random catchphrase key
-    const randomKey = catchphraseKeys[Math.floor(Math.random() * catchphraseKeys.length)];
-
-    // Get the catchphrase message based on the random key
-    const randomCatchphrase = catchphrases2[randomKey];
-
-    // Set the text content of the element to the random catchphrase
-    h2Element.textContent = randomCatchphrase;
-  }
-
-  // Call the function to inject a random catchphrase when the page loads
-  window.addEventListener('load', injectRandomCatchphrase2);
+setInterval(changeCatchphrase, 3000);
+const upBtn = document.querySelector(".fa-arrow-up");
+upBtn.addEventListener("click", () =>{
+  window.scrollTo({top: 0});
+})
